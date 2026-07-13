101bc262f0038e516b3e3f8830626f55e3010b2f
Pengcheng Wang <39308215+ChulanZhang@users.noreply.github.com>
2026-07-13 19:46:11 -0400

    Time: 3 ms (72.82%), Space: 21.3 MB (76.82%) - LeetHub

---
+++ 0252-meeting-rooms/0252-meeting-rooms.py
@@ -0,0 +1,12 @@
+class Solution:
+    def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
+        # Sort based on start time and then check adjcent meetings
+        intervals.sort()
+
+        for i in range(0, len(intervals) - 1):
+            if intervals[i][1] > intervals[i + 1][0]:
+                return False
+        return True
+
+
+        
