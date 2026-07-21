05c0d8f5d5267d8fd1657c551361b727ed9df5e8
Pengcheng Wang <39308215+ChulanZhang@users.noreply.github.com>
2026-07-21 11:15:03 -0400

    Time: 59 ms (33.44%), Space: 28.6 MB (75.19%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,11 +1,15 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        l, r = 0, 1
-        max_profit = 0
-        while r <= len(prices) - 1:
-            if prices[r] > prices[l]:
-                max_profit = max(max_profit, prices[r] - prices[l])
+        left, right = 0, 1
+        maxProfit = 0
+        while right < len(prices):
+            # check if it is profitable
+            profit = prices[right] - prices[left]
+            if profit > 0:
+                maxProfit = max(maxProfit, profit)
             else:
-                l = r
-            r += 1
-        return max_profit
+                # if it is not profitable, we move the left pointer to the position of the right pointer
+                left = right
+            right += 1
+        return maxProfit
+        
