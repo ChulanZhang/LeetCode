f720881f4e7205ceab9ef6f1f22997a09ac39205
Pengcheng Wang <39308215+ChulanZhang@users.noreply.github.com>
2026-08-07 16:45:09 -0400

    Time: 83 ms (6.76%), Space: 28.7 MB (40.69%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,15 +1,47 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        left, right = 0, 1
-        maxProfit = 0
+        # left represents the day on which we currently plan to buy.
+        # It always points to the lowest stock price seen before or at `right`.
+        left = 0
+
+        # right represents the day on which we are considering selling.
+        # Starting at index 1 guarantees that the selling day is after
+        # the buying day.
+        right = 1
+
+        # The maximum profit found so far.
+        # It starts at 0 because we can choose not to make a transaction
+        # when all prices are decreasing.
+        max_profit = 0
+
+        # Move the selling pointer through every possible selling day.
         while right < len(prices):
-            # check if it is profitable
-            profit = prices[right] - prices[left]
-            if profit > 0:
-                maxProfit = max(maxProfit, profit)
-            else:
-                # if it is not profitable, we move the left pointer to the position of the right pointer
+
+            # If today's price is lower than our current buying price,
+            # today is a better day to buy.
+            #
+            # For any future selling price:
+            #
+            # future_price - prices[right]
+            #
+            # will be greater than:
+            #
+            # future_price - prices[left]
+            #
+            # because prices[right] < prices[left].
+            #
+            # Therefore, the old buying day can never produce a better
+            # future profit, so we safely replace it with `right`.
+            if prices[right] < prices[left]:
                 left = right
+
+            else:
+                # Compare the profit from selling today with the best
+                # profit found from all previously examined selling days.
+                max_profit = max(max_profit, prices[right] - prices[left])
+
+            # Move to the next possible selling day.
             right += 1
-        return maxProfit
+
+        return max_profit
         
