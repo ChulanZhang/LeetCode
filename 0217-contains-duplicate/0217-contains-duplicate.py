3054978a3d0e78409557847a09b3f890081bd792
Pengcheng Wang <39308215+ChulanZhang@users.noreply.github.com>
2026-08-07 16:55:15 -0400

    Time: 11 ms (77.35%), Space: 32.3 MB (48.78%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,11 +1,11 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         # use hashtable to record the num and how mant times they appear in the array
-        nums_hashtable = {}
+        nums_seen = set()
         for num in nums:
-            if num in nums_hashtable.keys():
+            if num in nums_seen:
                 return True
             else:
-                nums_hashtable[num] = 1
+                nums_seen.add(num)
         return False
         
