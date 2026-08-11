64320d77f6d9f45e7cfe0eda03cd37600271ae0e
Pengcheng Wang <39308215+ChulanZhang@users.noreply.github.com>
2026-08-10 20:14:23 -0400

    Time: 3 ms (95.07%), Space: 20.3 MB (19.12%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -1,10 +1,23 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        ans = []
+        # For loop solution, based on Leetcode 191
+        # TC: O(nlogn)
+        # SC: O(1)
+        # ans = []
+        # for i in range(n + 1):
+        #     count = 0
+        #     while i:
+        #         # i & 1 will keep i's last bit
+        #         count += i & 1
+        #         # move i to the right by 1 bit, same as divided by 2
+        #         i >>= 1
+        #     ans.append(count)
+        # return ans
+
+        # DP solution: we can reuse the results from smaller numbers
+        dp = [0] * (n + 1)
         for i in range(n + 1):
-            count = 0
-            while i:
-                count += i & 1
-                i >>= 1
-            ans.append(count)
-        return ans
+            dp[i] = dp[i >> 1] + (i & 1)
+        
+        return dp
+
