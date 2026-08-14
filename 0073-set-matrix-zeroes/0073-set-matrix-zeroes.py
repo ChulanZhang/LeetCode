dabe92419a6c61a1d004a440e6f4b07249367224
Pengcheng Wang <39308215+ChulanZhang@users.noreply.github.com>
2026-08-14 11:05:23 -0400

    Time: 7 ms (57.6%), Space: 20.7 MB (47.85%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -3,15 +3,45 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
+        # Use the first row and first col to record
         m, n = len(matrix), len(matrix[0])
-        rows, cols = set(), set()
+
+        # Need to check if the first row and first col contains 0
+        first_row_zero = any(matrix[0][j] == 0 for j in range(n))
+        first_col_zero = any(matrix[i][0] == 0 for i in range(m))
+
         for i in range(m):
             for j in range(n):
                 if matrix[i][j] == 0:
-                    rows.add(i)
-                    cols.add(j)
+                    matrix[i][0], matrix[0][j] = 0, 0
         
-        for i in range(m):
-            for j in range(n):
-                if i in rows or j in cols:
+        for i in range(1, m):
+            for j in range(1, n):
+                if matrix[i][0] == 0 or matrix[0][j] == 0:
                     matrix[i][j] = 0
+        
+        if first_row_zero:
+            for j in range(n):
+                matrix[0][j] = 0
+        
+        if first_col_zero:
+            for i in range(m):
+                matrix[i][0] = 0
+
+
+
+#         m, n = len(matrix), len(matrix[0])
+#         rows, cols = set(), set()
+#         for i in range(m):
+#             for j in range(n):
+#                 if matrix[i][j] == 0:
+#                     rows.add(i)
+#                     cols.add(j)
+        
+#         for i in range(m):
+#             for j in range(n):
+#                 if i in rows or j in cols:
+#                     matrix[i][j] = 0
+
+# # TC: O(m*n)
+# # CS: O(m+n)
