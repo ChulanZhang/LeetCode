974a83528e4683599f6e83f6c4cf90ccb122e2bd
Pengcheng Wang <39308215+ChulanZhang@users.noreply.github.com>
2026-08-20 17:18:47 -0400

    Time: 0 ms (100%), Space: 22.4 MB (43.57%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.py
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.py
@@ -7,44 +7,44 @@
 class Solution:
     def findTarget(self, root: Optional[TreeNode], k: int) -> bool:
         # inorder method to collect all elements in the tree
-        nums = []
-        def inorder(node):
-            if not node:
-                return
-            inorder(node.left)
-            nums.append(node.val)
-            inorder(node.right)
-        
-        inorder(root)
-
-        left, right = 0, len(nums) - 1
-
-        while left < right:
-            total = nums[left] + nums[right]
-
-            if total == k:
-                return True
-            elif total > k:
-                right -= 1
-            else:
-                left += 1
-            
-        return False
-
-        # seen = set()
-
-        # def dfs(node):
+        # nums = []
+        # def inorder(node):
         #     if not node:
-        #         return False
-
-        #     if k - node.val in seen:
-        #         return True
-        #     else:
-        #         seen.add(node.val)
-            
-        #     return dfs(node.left) or dfs(node.right)
+        #         return
+        #     inorder(node.left)
+        #     nums.append(node.val)
+        #     inorder(node.right)
         
-        # return dfs(root)
+        # inorder(root)
+
+        # left, right = 0, len(nums) - 1
+
+        # while left < right:
+        #     total = nums[left] + nums[right]
+
+        #     if total == k:
+        #         return True
+        #     elif total > k:
+        #         right -= 1
+        #     else:
+        #         left += 1
+            
+        # return False
+
+        seen = set()
+
+        def dfs(node):
+            if not node:
+                return False
+
+            if k - node.val in seen:
+                return True
+            else:
+                seen.add(node.val)
+            
+            return dfs(node.left) or dfs(node.right)
+        
+        return dfs(root)
 
 # TC: O(n)
 # SC: O(n)
