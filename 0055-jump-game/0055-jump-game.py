58911daf15ad930ceb80f44c898e14b14eef7cd6
Pengcheng Wang <39308215+ChulanZhang@users.noreply.github.com>
2026-08-26 10:50:33 -0400

    Time: 23 ms (41.86%), Space: 20.2 MB (62.63%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,5 +1,18 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
+        max_reach_index = 0
+        for curr_index, curr_max_jump in enumerate(nums):
+            if curr_index > max_reach_index:
+                return False
+            max_reach_index = max(max_reach_index, curr_index + curr_max_jump)
+        return True
+
+        # goal = len(nums) - 1
+        # for curr_index, curr_jump in enumerate(len(nums) - 1, -1, -1):
+        #     if curr_index + curr_jump >= goal:
+        #         goal = curr_index
+        # return goal == 0
+
         # Solution 1
         # Greedy
         # max_reach >= len(nums) -1
@@ -10,13 +23,13 @@ class Solution:
         # max_reach = 3, max_reach = max(3, 2 + 1) = 3
         # max_reach = 3, max_reach = max(3, 3 + 0) = 3
         # max_reach = 3, curr_index = 4
-        max_reach_index = 0
-        for curr_index, curr_steps in enumerate(nums):
-            if curr_index > max_reach_index:
-                return False
-            max_reach_index = max(max_reach_index, curr_index + curr_steps)
-            # max_reach = max(0, 0 + 3)
-        return True
+        # max_reach_index = 0
+        # for curr_index, curr_steps in enumerate(nums):
+        #     if curr_index > max_reach_index:
+        #         return False
+        #     max_reach_index = max(max_reach_index, curr_index + curr_steps)
+        #     # max_reach = max(0, 0 + 3)
+        # return True
         # TC: O(n) for one for loop
         # SC: O(1)
 
