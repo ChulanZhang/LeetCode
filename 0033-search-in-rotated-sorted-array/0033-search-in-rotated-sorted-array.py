a98bd309f372df1db2a0a6e965cba8769aa971a9
Pengcheng Wang <39308215+ChulanZhang@users.noreply.github.com>
2026-09-02 11:11:59 -0400

    Time: 0 ms (100%), Space: 19.4 MB (39.02%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,5 +1,24 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = left + (right - left) // 2
+            if nums[mid] == target:
+                return mid
+            if nums[left] <= nums[mid]:
+                if nums[left] <= target < nums[mid]:
+                    right = mid -1
+                else:
+                    left = mid + 1
+            else:
+                if nums[mid] < target <= nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+        return -1
+
+
+'''
         # We use a CLOSED search interval:
         #
         # [left, right]
@@ -151,6 +170,5 @@ class Solution:
         # which means the search interval is empty.
         # Therefore target does not exist in nums.
         return -1
-
-
+'''
         
