e9b3b486900544c4cfb762a83d28aca00561bb81
Pengcheng Wang <39308215+ChulanZhang@users.noreply.github.com>
2026-09-10 20:58:19 -0400

    Time: 138 ms (41.32%), Space: 46.5 MB (98.77%) - LeetHub

---
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
@@ -0,0 +1,32 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        stack = [root]
+        parents = {root: None}
+
+        while p not in parents or q not in parents:
+            node = stack.pop()
+            if node.left:
+                parents[node.left] = node
+                stack.append(node.left)
+            if node.right:
+                parents[node.right] = node
+                stack.append(node.right)
+
+        p_ancestors = set()
+
+        while p:
+            p_ancestors.add(p)
+            p = parents[p]
+
+        while q not in p_ancestors:
+            q = parents[q]
+
+        return q
+        
