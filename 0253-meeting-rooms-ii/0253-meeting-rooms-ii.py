7f63af367f02bee241e42d6133cae4c6fca085e2
Pengcheng Wang <39308215+ChulanZhang@users.noreply.github.com>
2026-09-14 20:25:53 -0400

    Time: 3 ms (85.12%), Space: 21.4 MB (53.61%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
@@ -3,8 +3,8 @@ class Solution:
         start_time = sorted(i[0] for i in intervals)
         end_time = sorted(i[1] for i in intervals)
 
-        min_rooms = 0
-        start_pointer, end_pointer = 0, 0
+        min_rooms = 1
+        start_pointer, end_pointer = 1, 0
 
         while start_pointer < len(intervals):
             if start_time[start_pointer] < end_time[end_pointer]:
