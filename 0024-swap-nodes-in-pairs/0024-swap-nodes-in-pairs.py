da9dbf90a2e29bd1db40123ebf9d617de7af80e5
Pengcheng Wang <39308215+ChulanZhang@users.noreply.github.com>
2026-09-14 20:47:32 -0400

    Time: 0 ms (100%), Space: 19.3 MB (22.63%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.py
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.py
@@ -5,22 +5,28 @@
 #         self.next = next
 class Solution:
     def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # dami
         dummy = ListNode(0, head)
         prev = dummy
+        # we need two consecutive nodes to swap
         while prev.next and prev.next.next:
             # initialize
             first = prev.next
             second = prev.next.next
 
+            # Before swap:
             # prev -> first -> second -> next
+            # After swap:
             # prev -> second -> first -> next
 
-            # reset backwards 
+            # reset connections backwards 
             first.next = second.next
             second.next = first
             prev.next = second
 
             # move the prev by 2 steps
             prev = first
-    
         return dummy.next
+
+# TC: O(n) for one pass
+# SC: O(1) for dummy
