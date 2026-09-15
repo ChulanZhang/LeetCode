3f9ee0b5718f5cb9701e99583b3886847e6f3ded
Pengcheng Wang <39308215+ChulanZhang@users.noreply.github.com>
2026-09-14 20:31:54 -0400

    Time: 11 ms (6.91%), Space: 22.4 MB (6.99%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
@@ -1,18 +1,35 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        start_time = sorted(i[0] for i in intervals)
-        end_time = sorted(i[1] for i in intervals)
+        # sweep line method
+        max_rooms = 0
+        curr_rooms = 0
+        events = []
 
-        min_rooms = 1
-        start_pointer, end_pointer = 1, 0
+        for start, end in intervals:
+            events.append((start, 1))
+            events.append((end, -1))
 
-        while start_pointer < len(intervals):
-            if start_time[start_pointer] < end_time[end_pointer]:
-                min_rooms += 1
-            else:
-                end_pointer += 1
+        events.sort()
+
+        for time, delta in events:
+            curr_rooms += delta
+            max_rooms = max(max_rooms, curr_rooms)
+
+        return max_rooms
+
+#         start_time = sorted(i[0] for i in intervals)
+#         end_time = sorted(i[1] for i in intervals)
+
+#         min_rooms = 1
+#         start_pointer, end_pointer = 1, 0
+
+#         while start_pointer < len(intervals):
+#             if start_time[start_pointer] < end_time[end_pointer]:
+#                 min_rooms += 1
+#             else:
+#                 end_pointer += 1
             
-            start_pointer += 1
-        return min_rooms
-# TC: O(nlogn) for sorting
-# SC: O(n) for start_time and end_time
+#             start_pointer += 1
+#         return min_rooms
+# # TC: O(nlogn) for sorting
+# # SC: O(n) for start_time and end_time
