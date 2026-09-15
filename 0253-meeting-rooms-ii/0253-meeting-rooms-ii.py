ee2bfeeb2bb1922532969356d7c57af095d68049
Pengcheng Wang <39308215+ChulanZhang@users.noreply.github.com>
2026-09-14 20:18:10 -0400

    Time: 3 ms (85.12%), Space: 21.3 MB (79.66%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
@@ -1,9 +1,10 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        min_rooms = 1
         start_time = sorted(i[0] for i in intervals)
         end_time = sorted(i[1] for i in intervals)
-        start_pointer, end_pointer = 1, 0
+
+        min_rooms = 0
+        start_pointer, end_pointer = 0, 0
 
         while start_pointer < len(intervals):
             if start_time[start_pointer] < end_time[end_pointer]:
@@ -13,3 +14,5 @@ class Solution:
             
             start_pointer += 1
         return min_rooms
+# TC: O(nlogn) for sorting
+# SC: O(n) for start_time and end_time
