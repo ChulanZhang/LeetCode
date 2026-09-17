389b777a420bd96504ab587b250d29527ef3d898
Pengcheng Wang <39308215+ChulanZhang@users.noreply.github.com>
2026-09-17 00:09:37 -0400

    Time: 0 ms (100%), Space: 19.3 MB (24.28%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,26 +1,38 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        if len(s) % 2 != 0:
-            return False
         stack = []
+        p_map = {')': '(', '}': '{', ']': '['}
         for p in s:
-            if p in ['(', '{', '[']:
-                stack.append(p)
+            if p in p_map:
+                top_p = stack.pop() if stack else '#'
+                if p_map[p] != top_p:
+                    return False
             else:
-                if p == ')':
-                    if stack and stack[-1] == '(':
-                        stack.pop()
-                    else:
-                        return False
-                elif p == '}':
-                    if stack and stack[-1] == '{':
-                        stack.pop()
-                    else:
-                        return False
-                else:
-                    if stack and stack[-1] == '[':
-                        stack.pop()
-                    else:
-                        return False
+                stack.append(p)
         return len(stack) == 0
+
+
+        # if len(s) % 2 != 0:
+        #     return False
+        # stack = []
+        # for p in s:
+        #     if p in ['(', '{', '[']:
+        #         stack.append(p)
+        #     else:
+        #         if p == ')':
+        #             if stack and stack[-1] == '(':
+        #                 stack.pop()
+        #             else:
+        #                 return False
+        #         elif p == '}':
+        #             if stack and stack[-1] == '{':
+        #                 stack.pop()
+        #             else:
+        #                 return False
+        #         else:
+        #             if stack and stack[-1] == '[':
+        #                 stack.pop()
+        #             else:
+        #                 return False
+        # return len(stack) == 0
         
