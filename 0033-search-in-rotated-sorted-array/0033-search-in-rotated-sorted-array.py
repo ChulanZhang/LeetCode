cbf40f0ec3490ba4f1aac730d9cc953abd083b18
Pengcheng Wang <39308215+ChulanZhang@users.noreply.github.com>
2026-09-18 22:53:07 -0400

    Time: 0 ms (100%), Space: 19.4 MB (39.84%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -3,22 +3,58 @@ class Solution:
         left, right = 0, len(nums) - 1
         while left <= right:
             mid = left + (right - left) // 2
-            
             if nums[mid] == target:
                 return mid
-            
-            # left side is sorted
-            if nums[left] <= nums[mid]:
+            elif nums[left] <= nums[mid]:
                 if nums[left] <= target < nums[mid]:
                     right = mid - 1
                 else:
                     left = mid + 1
-            # right side is sorted
             else:
                 if nums[mid] < target <= nums[right]:
                     left = mid + 1
                 else:
                     right = mid - 1
         return -1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         
+
+        # left, right = 0, len(nums) - 1
+        # while left <= right:
+        #     mid = left + (right - left) // 2
+            
+        #     if nums[mid] == target:
+        #         return mid
+            
+        #     # left side is sorted
+        #     if nums[left] <= nums[mid]:
+        #         # see if target falls into this sorted part
+        #         if nums[left] <= target < nums[mid]:
+        #             right = mid - 1
+        #         else:
+        #             left = mid + 1
+        #     # right side is sorted
+        #     else:
+        #         # see if target falls into this sorted part
+        #         if nums[mid] < target <= nums[right]:
+        #             left = mid + 1
+        #         else:
+        #             right = mid - 1
+        # return -1
+# TC: O(logn)
+# SC: O(1) 
         
