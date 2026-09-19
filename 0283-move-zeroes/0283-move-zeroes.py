7343a7411408484ddad64915e498cd51d00c4f0f
Pengcheng Wang <39308215+ChulanZhang@users.noreply.github.com>
2026-09-18 22:49:11 -0400

    Time: 3 ms (81.84%), Space: 20.4 MB (90.11%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -9,10 +9,3 @@ class Solution:
                 nums[right], nums[left] = nums[left], nums[right]
                 left += 1
         return nums
-        # first_avail_idx = 0
-        # for i in range(len(nums)):
-        #     if nums[i] != 0:
-        #         nums[i], nums[first_avail_idx] = nums[first_avail_idx], nums[i]
-        #         first_avail_idx += 1
-        
-        
