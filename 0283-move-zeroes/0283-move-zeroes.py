db9bb820336ce90043344ec8e6e482234c3648f3
Pengcheng Wang <39308215+ChulanZhang@users.noreply.github.com>
2026-09-18 22:44:45 -0400

    Time: 4 ms (59.19%), Space: 20.6 MB (5.3%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,10 +3,16 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        first_avail_idx = 0
-        for i in range(len(nums)):
-            if nums[i] != 0:
-                nums[i], nums[first_avail_idx] = nums[first_avail_idx], nums[i]
-                first_avail_idx += 1
+        left = 0
+        for right in range(len(nums)):
+            if nums[right] != 0:
+                nums[right], nums[left] = nums[left], nums[right]
+                left += 1
+        return nums
+        # first_avail_idx = 0
+        # for i in range(len(nums)):
+        #     if nums[i] != 0:
+        #         nums[i], nums[first_avail_idx] = nums[first_avail_idx], nums[i]
+        #         first_avail_idx += 1
         
         
