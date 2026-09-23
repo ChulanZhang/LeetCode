8e575b58a1f59c7571ea1e71768bf02a796ab9a1
Pengcheng Wang <39308215+ChulanZhang@users.noreply.github.com>
2026-09-23 00:12:21 -0400

    Time: 0 ms (100%), Space: 20 MB (62.09%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
@@ -6,23 +6,35 @@
 #         self.right = right
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        if not root:
-            return []
         results = []
-        def bfs(nodes):
-            if not nodes:
+        def dfs(node, level):
+            if not node:
                 return
-            tmp = []
-            next_level = []
-            for node in nodes:
-                tmp.append(node.val)
-                if node.left:
-                    next_level.append(node.left)
-                if node.right:
-                    next_level.append(node.right)
-            results.append(tmp)
-
-            bfs(next_level)
-
-        bfs([root])
+            if len(results) == level:
+                results.append([])
+            results[level].append(node.val)
+            dfs(node.left, level + 1)
+            dfs(node.right, level + 1)
+        
+        dfs(root, 0)
         return results
+        # if not root:
+        #     return []
+        # results = []
+        # def bfs(nodes):
+        #     if not nodes:
+        #         return
+        #     tmp = []
+        #     next_level = []
+        #     for node in nodes:
+        #         tmp.append(node.val)
+        #         if node.left:
+        #             next_level.append(node.left)
+        #         if node.right:
+        #             next_level.append(node.right)
+        #     results.append(tmp)
+
+        #     bfs(next_level)
+
+        # bfs([root])
+        # return results
